fb10401fa58123a5177e9be22e83d8b1c87e465b
Marcelo Israel Pérez Villa <74079519+MarceloIPV@users.noreply.github.com>
2023-03-03 16:53:09 -0500

    Commit Tabla

---
+++ layout/scripts/js/PAID_DESARROLLO_JS/datatablets.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+
+$(".body__tablas__anexos").append('<tr id="'+noVariable+'"><td>'+elemento[4]+'</td><td>'+elemento[5]+'</td><td>'+elemento[1]+'</td><td>'+parseFloat(elemento[2]).toFixed(2)+'</td><td>'+parseFloat(elemento[3]).toFixed(2)+'</td><td><button class="btn btn-warning" id="destinoU'+noVariable+'">Destino</button></td></tr>');
+
+});
