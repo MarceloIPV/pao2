2eb07c873c05de4c8f9f7af943fd97a767e45b8a
DavidPaca <david.paca1992@gmail.com>
2023-03-08 17:11:14 -0500

    Actualizacion creacio de selectorjs

--- vistas/vistasGenerales/header.view.php
+++ vistas/vistasGenerales/header.view.php
@@ -235,6 +235,7 @@
 
 
 		<script type="text/javascript" src="layout/scripts/js/PAID_DESARROLLO_JS/index.js"></script>
+		<script type="text/javascript" src="layout/scripts/js/paid-alto-rendimiento-desarrollo/index.js"></script>
 
 			
 		<!--====  End of Scripts personalizados  ====-->
